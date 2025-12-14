b753e2167d81fb62245becf4efb015c54ebafe80
Dimos Tzimotoudis <dtzimotoudis@gmail.com>
2025-12-14 18:21:32 +0000

    update sertup arguments

--- setup.sh
+++ setup.sh
@@ -1,8 +1,31 @@
 #!/bin/bash
 set -e
 
-TOOL="$2"
-DOCKER_FLAG="$3"
+TOOL=""
+DOCKER_FLAG=""
+
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        --tool)
+            TOOL="$2"
+            shift 2
+            ;;
+        --docker)
+            DOCKER_FLAG="--docker"
+            shift
+            ;;
+        *)
+            echo "Unknown argument: $1"
+            exit 1
+            ;;
+    esac
+done
+
+if [ -z "$TOOL" ]; then
+    echo "Error: --tool is required"
+    echo "Usage: ./setup.sh --tool <tool_name> [--docker]"
+    exit 1
+fi
 
 cd "$(dirname "$0")/tools/$TOOL"
 ./setup.sh $DOCKER_FLAG
